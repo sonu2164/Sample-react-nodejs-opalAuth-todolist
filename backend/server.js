e298ffd139959bfa9a1cddf8d04aa7a377333060
Sonu Singh <102368177+sonu2164@users.noreply.github.com>
2024-12-24 11:07:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,6 @@ app.use(express.json({ extended: false }));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
